93aa4b7a688eee67ea405324c87342ea873cb68c
vivek536 <vivek536@users.noreply.github.com>
2020-01-14 14:36:47 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,34 @@
+class Student{  
+    int id;  
+    String name;  
+    int age;
+    String addr;  
+    //creating two arg constructor  
+    public Student(int i,String n){  
+    id = i;  
+    name = n;  
+    }  
+    //creating three arg constructor  
+    Student(int i,String n,int a){  
+    id = i;  
+    name = n;  
+    age=a;  
+    }  
+    protected Student(int id,String n,int a,String addr){
+       this.id=id;
+       name=n;
+       age=a;
+       this.addr=addr;
+}
+    void display(){System.out.println(id+" "+name+" "+age+addr);}  
+   
+    public static void main(String args[]){  
+    Student s1 = new Student(111,"Karan");  
+    Student s2 = new Student(222,"Aryan",25);  
+    Student s3 = new Student(333,"Arnold",28,"Gachibowli");
+    s1.display();  
+    s2.display();  
+s3.display();
+
+   }  
+}  
