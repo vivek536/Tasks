aa55501e059abcd6a9de77f978adde241ff83d15
GnanaVivek Ponnuru <Gnanavivek_Ponnuru@epam.com>
2020-02-04 15:32:37 +0530

    Log4net

---
+++ Log4net.Library/Global.asax.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+using log4net;
+
+namespace Log4net.Library
+{
+    public class Global : HttpApplication
+    {
+       
+            log4net.ILog logger = log4net.LogManager.GetLogger(typeof(Global));  //Declaring Log4Net
+
+        void Application_Start(object sender, EventArgs e)
+        {
+            // Code that runs on application startup
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            log4net.Config.XmlConfigurator.Configure();
+            logger.Error("Vivek");
+           
+        }
+    }
+}
